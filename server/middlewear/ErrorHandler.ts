dd42e769524b5464d9f7805a5bd254029fc1bd96
Benedict <benedict.arowo@gmail.com>
2023-11-07 19:21:01 +0300

    + Error Handler

---
+++ server/middlewear/ErrorHandler.ts
@@ -0,0 +1,35 @@
+// Inside the error, I'm expecting
+// Error Message
+// An error code
+
+import { NextFunction, Request, Response } from "express";
+import CustomError from "./CustomError";
+import dotenv from "dotenv";
+dotenv.config();
+
+const ErrorHandler = (
+	err: any,
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	let { code, message } = err;
+
+	// If the error is not an instance of the CustomError class meaning it won't have a code property.
+	if (!(err instanceof CustomError)) {
+		console.log(1);
+		return res.status(404).json({
+			success: false,
+			message: process.env.DEFAULT_ERROR_MESSAGE,
+			stack: process.env.NODE_ENV === "dev" ? err.stack : null,
+		});
+	}
+	console.log(2);
+	return res.status(code || 404).json({
+		success: false,
+		message: message || process.env.DEFAULT_ERROR_MESSAGE,
+		stack: process.env.NODE_ENV === "dev" ? err.stack : null,
+	});
+};
+
+export default ErrorHandler;
