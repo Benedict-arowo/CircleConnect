3f2e544c895e8ba34583efa46d29f1253a8abf5d
Benedict <benedict.arowo@gmail.com>
2023-11-07 19:40:13 +0300

    Created and implemented wrapper function

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,22 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import wrapper from "./middlewear/wrapper";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use('', morgan('dev'))
+app.use("", morgan("dev"));
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({ msg: 'Hello World!' })
-});
+app.get(
+	"/",
+	wrapper((req: Request, res: Response) => {
+		throw new Error("Test");
+		res.json({ msg: "Hello World!" });
+	})
+);
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
