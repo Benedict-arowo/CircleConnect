3d44e7c354b4969d289cfbc66dcf3371db6fd6e7
Benedict <benedict.arowo@gmail.com>
2023-11-09 11:06:41 +0300

    Minor changes

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+
 const Index = () => {
-	return <div>Circle Connect</div>;
+	const [user, setUser] = useState(null);
+
+	const fetchUser = () => {
+		fetch("http://localhost:8000/user", {
+			credentials: "include",
+			headers: {
+				Accept: "application/json",
+			},
+		})
+			.then((response) => {
+				console.log(response);
+				if (!response.ok) throw new Error(response.statusText);
+				return response.json();
+			})
+			.then((data) => {
+				setUser(data);
+			})
+			.catch((error) => {
+				setUser(null);
+				console.log(error);
+			});
+	};
+
+	useEffect(() => {
+		fetchUser();
+	}, []);
+
+	console.log(user);
+	return (
+		<div>
+			{user && (
+				<div>
+					<h1>
+						Welcome {user.first_name}, your user id is {user.id}
+					</h1>
+					<a href="http://localhost:8000/logout">Logout</a>
+				</div>
+			)}
+			{!user && (
+				<div>
+					<p>You are currently not logged in.</p>
+					<a href="/auth">Login here</a>
+				</div>
+			)}
+			<h1 className="text-2xl font-bold w-full text-center">
+				Circle Connect
+			</h1>
+		</div>
+	);
 };
 
 export default Index;
