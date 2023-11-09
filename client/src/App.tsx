da63671b1e16ae53b235b03f74ac8c48b51f9b38
Benedict <benedict.arowo@gmail.com>
2023-11-09 10:55:40 +0300

    Added success pages, and routes for oAuth logins

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Index from "./pages/Index";
+import Success from "./pages/Auth/Success";
 
 const App = () => {
 	return (
@@ -8,6 +9,8 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<Index />} />
 				<Route path="/auth" element={<Auth />} />
+				<Route path="/auth/google/success" element={<Success />} />
+				<Route path="/auth/github/success" element={<Success />} />
 			</Routes>
 		</>
 	);
---
+++ client/src/pages/Auth/Success.tsx
@@ -0,0 +1,9 @@
+const Success = () => {
+	setInterval(() => {
+		window.close();
+	}, 1000);
+
+	return <div>success</div>;
+};
+
+export default Success;
