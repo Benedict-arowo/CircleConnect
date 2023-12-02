4d393b293e24ed4682ce7d76c8ad00f649a498cd
Benedict <benedict.arowo@gmail.com>
2023-12-02 15:40:40 +0300

    React Redux User State Configuration

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { increment, decrement } from "./Auth/userSlice";
+import { loginUser } from "./Auth/userSlice";
+// import { increment, decrement } from "./Auth/userSlice";
 const Test = () => {
 	const dispatch = useDispatch();
-	const state = useSelector((state) => state.user.count);
+	const state = useSelector((state) => state.user);
+	console.log(state);
 	return (
 		<div>
-			<p>{state}</p>
-			<p onClick={() => dispatch(increment())}>+</p>
-			<p onClick={() => dispatch(decrement())}>-</p>
+			{/* <p>{state}</p> */}
+			<p onClick={() => dispatch(loginUser("123"))}>+</p>
+			{/* <p onClick={() => dispatch(decrement())}>-</p> */}
 			<a href="/">Home</a>
 		</div>
 	);
