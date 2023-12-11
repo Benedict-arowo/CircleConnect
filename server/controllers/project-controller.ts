758d569a0812337ac81cf8e32ac62ae495213414
Benedict <benedict.arowo@gmail.com>
2023-12-11 15:19:59 +0300

    getProjects controller

---
+++ server/controllers/project-controller.ts
@@ -0,0 +1,88 @@
+import { StatusCodes } from "http-status-codes";
+import { Req } from "../types";
+import { Response } from "express";
+import CustomError from "../middlewear/CustomError";
+import { UserSelectMinimized } from "../utils";
+import prisma from "../model/db";
+
+export const getProjects = async (req: Req, res: Response) => {
+	const { limit = "10", sortedBy, userId, circleId } = req.query;
+	const sortedByValues = [
+		"circle_id-asc",
+		"circle_id-desc",
+		"name-asc",
+		"name-desc",
+	];
+
+	if (isNaN(parseInt(limit)))
+		throw new CustomError(
+			"Invalid limit provided",
+			StatusCodes.BAD_REQUEST
+		);
+
+	if (sortedBy && !sortedByValues.includes(sortedBy)) {
+		throw new CustomError(
+			"Invalid sorting parameters",
+			StatusCodes.BAD_REQUEST
+		);
+	}
+
+	if (Number(limit) > 25 || Number(limit) < 1)
+		throw new CustomError(
+			"Invalid limit, must be between 1 and 25",
+			StatusCodes.BAD_REQUEST
+		);
+
+	console.log(
+		circleId
+			? !isNaN(Number(circleId))
+				? Number(circleId)
+				: undefined
+			: undefined
+	);
+	const Projects = await prisma.project.findMany({
+		where: {
+			OR: circleId
+				? [
+						{
+							circleId: circleId
+								? !isNaN(Number(circleId))
+									? Number(circleId)
+									: undefined
+								: undefined,
+						},
+						// {
+						// 	createdById: userId ? userId : undefined,
+						// },
+				  ]
+				: undefined,
+		},
+		orderBy: {
+			circleId: sortedBy?.startsWith("circle_id")
+				? sortedBy === "circle_id-asc"
+					? "asc"
+					: "desc"
+				: undefined,
+			name: sortedBy?.startsWith("name")
+				? sortedBy === "name-asc"
+					? "asc"
+					: "desc"
+				: undefined,
+		},
+		select: {
+			name: true,
+			description: true,
+			circle: true,
+			createdAt: true,
+			createdBy: {
+				select: UserSelectMinimized,
+			},
+			liveLink: true,
+			github: true,
+			id: true,
+		},
+		take: limit ? parseInt(limit) : undefined,
+	});
+
+	res.status(StatusCodes.OK).json({ success: true, data: Projects });
+};
