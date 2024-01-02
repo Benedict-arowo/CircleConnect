24e3872d3ba2e7be95a82209e255ab0f1e38dc77
Benedict <benedict.arowo@gmail.com>
2024-01-02 21:27:59 +0300

    REFACTOR: Changed Manage Members button to Members so everyone can view list of circle members.

--- client/src/pages/Circle.tsx
+++ client/src/pages/Circle.tsx
@@ -530,102 +530,93 @@ const Circle = () => {
 								)}
 							</section>
 							<p className="h-[100px]">{circle.description}</p>
-							{User.isLoggedIn && (
-								<div
-									className={`flex flex-row ${
-										state.isOwner || state.isMember
-											? "justify-between"
-											: "justify-end"
-									}`}>
-									<div className="flex flex-row gap-3">
-										{state.isOwner && (
-											<div className="flex flex-row gap-3">
-												<button
-													onClick={
-														settingsDrawerOnOpen
-													}
-													className="text-gray-500 bg-green-500 text-base rounded-sm hover:bg-gray-500 hover:text-white bg-transparent border border-gray-800 duration-300 px-8 py-1">
-													Settings
-												</button>
-												<button
-													onClick={drawerOnOpen}
-													className="text-gray-500 bg-green-500 text-base rounded-sm hover:bg-gray-500 hover:text-white bg-transparent border border-gray-800 duration-300 px-8 py-1">
-													Manage Members
-												</button>
-											</div>
-										)}
-										{state.isMember && (
+							<div className={`flex flex-row justify-between`}>
+								<div className="flex flex-row gap-3">
+									{state.isOwner && (
+										<div className="flex flex-row gap-3">
 											<button
-												onClick={projectDrawerOnOpen}
+												onClick={settingsDrawerOnOpen}
 												className="text-gray-500 bg-green-500 text-base rounded-sm hover:bg-gray-500 hover:text-white bg-transparent border border-gray-800 duration-300 px-8 py-1">
-												Add Project
+												Settings
 											</button>
-										)}
-									</div>
-
-									<div className="flex flex-row gap-4">
-										{/* REPORT ICON */}
-										{!state.isOwner && (
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												strokeWidth={1.5}
-												stroke="currentColor"
-												className="w-6 h-6 cursor-pointer text-gray-800 hover:text-gray-950 transition-all duration-300">
-												<path
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
-												/>
-											</svg>
-										)}
-										{/* DELETE ICON */}
-										{state.isOwner && (
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												fill="none"
-												viewBox="0 0 24 24"
-												strokeWidth={1.5}
-												stroke="currentColor"
-												onClick={() => {
-													setAlertState(() => {
-														return {
-															body: "Are you sure you want to delete this circle? You can't undo this action afterwards.",
-															doneText:
-																"Delete Circle",
-															header: "Delete Circle",
-															doneFunc: () =>
-																makeReq({
-																	url: `circle/${circle.id}`,
-																	method: "DELETE",
-																	loadingMsg:
-																		"Please wait while we try to delete the circle.",
-																	successMsg:
-																		"Successfully deleted the circle.",
-																	successFunc:
-																		() => {
-																			onClose();
-																			Navigate(
-																				"/"
-																			);
-																		},
-																}),
-														};
-													});
-													onOpen();
-												}}
-												className="w-6 h-6 cursor-pointer text-gray-800 hover:text-red-600 transition-all duration-300">
-												<path
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-												/>
-											</svg>
-										)}
-									</div>
+										</div>
+									)}
+									<button
+										onClick={drawerOnOpen}
+										className="text-gray-500 bg-green-500 text-base rounded-sm hover:bg-gray-500 hover:text-white bg-transparent border border-gray-800 duration-300 px-8 py-1">
+										Members
+									</button>
+									{state.isMember && (
+										<button
+											onClick={projectDrawerOnOpen}
+											className="text-gray-500 bg-green-500 text-base rounded-sm hover:bg-gray-500 hover:text-white bg-transparent border border-gray-800 duration-300 px-8 py-1">
+											Add Project
+										</button>
+									)}
 								</div>
-							)}
+
+								<div className="flex flex-row gap-4">
+									{/* REPORT ICON */}
+									{!state.isOwner && (
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											className="w-6 h-6 cursor-pointer text-gray-800 hover:text-gray-950 transition-all duration-300">
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M3 3v1.5M3 21v-6m0 0l2.77-.693a9 9 0 016.208.682l.108.054a9 9 0 006.086.71l3.114-.732a48.524 48.524 0 01-.005-10.499l-3.11.732a9 9 0 01-6.085-.711l-.108-.054a9 9 0 00-6.208-.682L3 4.5M3 15V4.5"
+											/>
+										</svg>
+									)}
+									{/* DELETE ICON */}
+									{state.isOwner && (
+										<svg
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											strokeWidth={1.5}
+											stroke="currentColor"
+											onClick={() => {
+												setAlertState(() => {
+													return {
+														body: "Are you sure you want to delete this circle? You can't undo this action afterwards.",
+														doneText:
+															"Delete Circle",
+														header: "Delete Circle",
+														doneFunc: () =>
+															makeReq({
+																url: `circle/${circle.id}`,
+																method: "DELETE",
+																loadingMsg:
+																	"Please wait while we try to delete the circle.",
+																successMsg:
+																	"Successfully deleted the circle.",
+																successFunc:
+																	() => {
+																		onClose();
+																		Navigate(
+																			"/"
+																		);
+																	},
+															}),
+													};
+												});
+												onOpen();
+											}}
+											className="w-6 h-6 cursor-pointer text-gray-800 hover:text-red-600 transition-all duration-300">
+											<path
+												strokeLinecap="round"
+												strokeLinejoin="round"
+												d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+											/>
+										</svg>
+									)}
+								</div>
+							</div>
 						</header>
 						<section className="mt-16">
 							<div className="flex flex-row gap-2 items-center">
