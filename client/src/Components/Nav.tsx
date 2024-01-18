89b0eb80730a2ff5da59585220ce2b8357b818b0
Benedict <benedict.arowo@gmail.com>
2024-01-18 20:55:35 +0300

    REFACTOR: Notification re-design

--- client/src/Components/Nav.tsx
+++ client/src/Components/Nav.tsx
@@ -143,14 +143,20 @@ const Nav = (props: Props) => {
 												<div className="bg-red-500 rounded-full text-xs py-1 px-1 absolute bottom-3 right-4"></div>
 											</Button>
 										</PopoverTrigger>
-										<PopoverContent className="mx-4">
+										<PopoverContent className="mx-2">
 											<PopoverArrow />
-											{/* <PopoverCloseButton />
+											<PopoverCloseButton />
 											<PopoverHeader>
-												Notifications
-											</PopoverHeader> */}
+												<h4 className="font-light text-sm text-center">
+													Notifications (
+													<span className="text-red-500">
+														{notifications.length}
+													</span>
+													)
+												</h4>
+											</PopoverHeader>
 											<PopoverBody>
-												<div className="flex flex-col gap-2">
+												<div className="flex flex-col">
 													{notifications.length ===
 														0 && (
 														<p>
@@ -166,22 +172,65 @@ const Nav = (props: Props) => {
 																	content,
 																	status,
 																	url,
+																	user,
 																} =
 																	notification;
 																return (
-																	<a
-																		href={
-																			url
-																		}
-																		key={
-																			id
-																		}>
-																		{
-																			content
-																		}{" "}
-																		|{" "}
-																		{status}
-																	</a>
+																	<div
+																		key={id}
+																		className={`px-2 py-3 flex flex-row gap-3 items-center border-b border-b-slate-200 border-1 hover:bg-slate-100 duration-300 cursor-pointer bg-white}`}>
+																		<Avatar
+																			name={`${user.first_name}`}
+																			src={
+																				user.profile_picture
+																			}
+																			width="32px"
+																			height="32px"
+																			className="cursor-pointer"
+																			colorScheme="teal"
+																		/>
+																		<a
+																			href={
+																				url
+																			}
+																			className="font-light">
+																			{
+																				content
+																			}
+																		</a>
+																		{/* <div className="flex flex-row gap-2">
+																			<svg
+																				xmlns="http://www.w3.org/2000/svg"
+																				fill="none"
+																				viewBox="0 0 24 24"
+																				strokeWidth={
+																					1.5
+																				}
+																				stroke="currentColor"
+																				className="w-4 h-4">
+																				<path
+																					strokeLinecap="round"
+																					strokeLinejoin="round"
+																					d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+																				/>
+																			</svg>
+																			<svg
+																				xmlns="http://www.w3.org/2000/svg"
+																				fill="none"
+																				viewBox="0 0 24 24"
+																				strokeWidth={
+																					1.5
+																				}
+																				stroke="currentColor"
+																				className="w-4 h-4">
+																				<path
+																					strokeLinecap="round"
+																					strokeLinejoin="round"
+																					d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+																				/>
+																			</svg>
+																		</div> */}
+																	</div>
 																);
 															}
 														)}
