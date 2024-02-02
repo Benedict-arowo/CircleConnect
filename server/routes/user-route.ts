df03a5e948f95e053bb50ae1ee046ab870872873
Benedict <benedict.arowo@gmail.com>
2024-02-03 02:11:15 +0300

    ADD: User route, and controller. Get, Delete, and Edit user fully functional

---
+++ server/routes/user-route.ts
@@ -0,0 +1,23 @@
+import {
+	// createUser,
+	deleteUser,
+	editUser,
+	getUser,
+	getUsers,
+} from "../controllers/user-controller";
+import isLoggedIn from "../middlewear/isLoggedIn";
+import wrapper from "../middlewear/wrapper";
+
+const express = require("express");
+const userRouter = express.Router();
+
+userRouter.route("/").get(wrapper(getUsers));
+// .post(isLoggedIn, wrapper(createUser));
+
+userRouter
+	.route("/:id")
+	.get(wrapper(getUser))
+	.patch(isLoggedIn, wrapper(editUser))
+	.delete(isLoggedIn, wrapper(deleteUser));
+
+export default userRouter;
