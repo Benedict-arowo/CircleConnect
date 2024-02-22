b5ce23be56bcca5285e81f83963df8d1af57683f
Benedict <benedict.arowo@gmail.com>
2024-02-22 14:50:39 +0300

    Update Roles component with new role creation functionality

--- client/src/pages/Dashboard/Roles.tsx
+++ client/src/pages/Dashboard/Roles.tsx
@@ -8,7 +8,7 @@ import {
 	TableRow,
 } from "@mui/material";
 import { Button } from "@chakra-ui/react";
-import { useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import { UserTypeClean } from "../../types";
 import UseFetch from "../../Components/Fetch";
 import { DEFAULT_MEMBER_ROLE_ID } from "../../../config";
@@ -67,6 +67,29 @@ type ArrangedRoleData = {
 	users: UserTypeClean[];
 };
 
+const defaultRoleData = {
+	name: "",
+	permissions: {
+		canCreateCircle: false,
+		canModifyOwnCircle: false,
+		canModifyOtherCircle: false,
+		canDeleteOwnCircle: false,
+		canDeleteOtherCircles: false,
+		canLeaveCircle: false,
+		canJoinCircle: false,
+		canCreateProject: false,
+		canModifyOwnProject: false,
+		canModifyOtherProject: false,
+		canDeleteOwnProject: false,
+		canDeleteOtherProject: false,
+		canAddProjectToCircle: false,
+		canRemoveProjectFromCircle: false,
+		canManageRoles: false,
+		canManageUsers: false,
+		isAdmin: false,
+	},
+};
+
 const Roles = () => {
 	const [data, setData] = useState<Role[]>([]);
 	const [editRoleDialogIsVisible, setEditRoleDialogIsVisible] =
@@ -74,6 +97,9 @@ const Roles = () => {
 
 	const [editData, setEditData] = useState<ArrangedRoleData | null>(null);
 	const [displayMemberMenu, setDisplayMemberMenu] = useState(false);
+	const [createRoleData, setCreateRoleData] = useState(defaultRoleData);
+	const [newRoleDialog, setNewRoleDialog] = useState(false);
+
 	const toast = useRef<Toast | null>(null);
 
 	const fetchRoles = async () => {
@@ -134,8 +160,10 @@ const Roles = () => {
 
 	const DisplayPermissions = ({
 		permissions,
+		updateFunc,
 	}: {
 		permissions: ArrangedRoleData["permissions"];
+		updateFunc: Dispatch<SetStateAction<ArrangedRoleData | null>>;
 	}) => {
 		const permList = [];
 
@@ -146,12 +174,25 @@ const Roles = () => {
 		return (
 			<div className="flex flex-col gap-1">
 				{permList.map((perm) => {
+					function convertPascalToNormal(pascalCase) {
+						// Add a space before each capital letter, except for the first one
+						const normalCase = pascalCase.replace(
+							/([A-Z])/g,
+							" $1"
+						);
+
+						// Capitalize the first letter and remove leading space
+						return (
+							normalCase.charAt(0).toUpperCase() +
+							normalCase.slice(1).trim()
+						);
+					}
 					return (
 						<div
 							key={perm}
 							className="flex flex-row justify-between text-sm px-2 py-1"
 						>
-							<h4>{perm}</h4>
+							<h4>{convertPascalToNormal(perm)}</h4>
 
 							<InputSwitch
 								checked={
@@ -160,7 +201,7 @@ const Roles = () => {
 									]
 								}
 								onChange={(e) =>
-									setEditData(
+									updateFunc(
 										(prev: ArrangedRoleData | null) => {
 											if (!prev) return null;
 											return {
@@ -241,7 +282,7 @@ const Roles = () => {
 			detail: "Successfully saved changes...",
 			life: 3000,
 		});
-		closeNewRoleDialog();
+		closeEditRoleDialog();
 	};
 
 	const DeleteRole = async (roleId: string) => {
@@ -284,10 +325,58 @@ const Roles = () => {
 			detail: "Successfully deleted role...",
 			life: 3000,
 		});
-		closeNewRoleDialog();
+		closeEditRoleDialog();
 	};
 
-	const closeNewRoleDialog = () => {
+	const CreateRole = async () => {
+		toast.current?.show({
+			severity: "info",
+			summary: "Loading...",
+			detail: "Creating role...",
+			life: 3000,
+		});
+
+		const { data: fetchData, response } = await UseFetch({
+			url: `role`,
+			options: {
+				body: createRoleData,
+				method: "POST",
+				returnResponse: true,
+				useServerUrl: true,
+			},
+		});
+
+		if (!response.ok)
+			// Displays the error message gotten back from the server, and if there isn't one, it uses a generic message.
+			return toast.current?.show({
+				severity: "error",
+				summary: "Oops..",
+				detail: `${
+					fetchData && fetchData.message
+						? fetchData.message
+						: "Error trying to communicate with the server."
+				}`,
+				life: 3000,
+			});
+
+		// Add the new role to the local role data list
+		setData((prev) => {
+			return [fetchData.data, ...prev];
+		});
+
+		setNewRoleDialog(false);
+		// Resets the create form
+		setCreateRoleData(() => defaultRoleData);
+
+		toast.current?.show({
+			severity: "success",
+			summary: "Role Created!",
+			detail: "Successfully created role...",
+			life: 3000,
+		});
+	};
+
+	const closeEditRoleDialog = () => {
 		setEditRoleDialogIsVisible(false);
 		setTimeout(() => {
 			setEditData(null);
@@ -309,7 +398,11 @@ const Roles = () => {
 			<div className="flex flex-row text-center mt-10 gap-8 w-full justify-between">
 				<section className="flex flex-row items-center justify-between w-full gap-8">
 					<h1 className="text-4xl font-bold">Roles</h1>
-					<i className="pi pi-plus bg-yellow-400 text-white px-2 py-2 rounded-full"></i>
+					<i
+						onClick={() => setNewRoleDialog(true)}
+						title="Create a new role."
+						className="pi pi-plus cursor-pointer shadow-lg hover:scale-105 duration-200 bg-yellow-400 text-white px-2 py-2 rounded-full"
+					></i>
 				</section>
 				<Button
 					colorScheme="yellow"
@@ -350,7 +443,7 @@ const Roles = () => {
 									>
 										<TableCell>{row.name}</TableCell>
 										<TableCell>
-											{row.users.length}
+											{row.users ? row.users.length : 0}
 										</TableCell>
 										<TableCell>
 											{row.isAdmin && (
@@ -393,7 +486,7 @@ const Roles = () => {
 				visible={editRoleDialogIsVisible}
 				style={{ width: "50vw" }}
 				dismissableMask
-				onHide={closeNewRoleDialog}
+				onHide={closeEditRoleDialog}
 				draggable={false}
 			>
 				{editData && (
@@ -450,6 +543,7 @@ const Roles = () => {
 							{
 								<DisplayPermissions
 									permissions={editData.permissions}
+									updateFunc={setEditData}
 								/>
 							}
 						</section>
@@ -517,6 +611,77 @@ const Roles = () => {
 						</footer>
 					</div>
 				)}
+			</Dialog>
+			<Dialog
+				header={`New Role ${
+					createRoleData.name && `- ${createRoleData.name}`
+				}`}
+				visible={newRoleDialog}
+				style={{ width: "50vw" }}
+				dismissableMask
+				onHide={() => setNewRoleDialog(false)}
+				draggable={false}
+			>
+				<div>
+					<section>
+						<h3 className="font-bold text-sm">Info</h3>
+						<div className="px-2 flex flex-col gap-2 mt-2">
+							<span className="flex flex-row gap-2 items-center">
+								<label
+									htmlFor="role_name"
+									className="font-bold text-sm text-center border-1 border-zinc-300"
+								>
+									Name:
+								</label>
+								<InputText
+									id="role_name"
+									placeholder="Role Name"
+									value={createRoleData.name}
+									className="w-full"
+									onChange={(e) =>
+										setCreateRoleData((prev) => {
+											return {
+												...prev,
+												name: e.target.value,
+											};
+										})
+									}
+								/>
+							</span>
+						</div>
+					</section>
+					<section className="mt-3">
+						<h3 className="font-bold text-sm">Permissions</h3>
+						{
+							<DisplayPermissions
+								permissions={createRoleData.permissions}
+								updateFunc={
+									setCreateRoleData as Dispatch<
+										SetStateAction<ArrangedRoleData | null>
+									>
+								}
+							/>
+						}
+					</section>
+
+					<footer className="flex flex-row gap-6 justify-center mt-4">
+						<button
+							onClick={() =>
+								confirmDialog({
+									message:
+										"Are you sure you want to proceed?",
+									header: "Confirmation",
+									icon: "pi pi-exclamation-triangle",
+									defaultFocus: "accept",
+									accept: CreateRole,
+								})
+							}
+							className="w-fit h-fit bg-green-600 px-4 py-1 text-white rounded-md font-normal"
+						>
+							Create Role
+						</button>
+					</footer>
+				</div>
 			</Dialog>
 		</div>
 	);
