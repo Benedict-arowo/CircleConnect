0e1c91fda559c3fcfcd69e48051021b1125e81b7
Benedict <benedict.arowo@gmail.com>
2024-03-05 10:31:59 +0300

    Refactor lazy loading and add loading spinner

--- client/src/Components/Loading.tsx
+++ client/src/Components/Loading.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { ProgressSpinner } from "primereact/progressspinner";
 
 const Loading = () => {
-	return <div>Loading...</div>;
+	return (
+		<div className="flex h-screen w-full justify-center items-center">
+			<ProgressSpinner
+				style={{ width: "50px", height: "50px" }}
+				strokeWidth="8"
+				fill="var(--surface-ground)"
+				animationDuration=".5s"
+			/>
+		</div>
+	);
 };
 
 export default Loading;
