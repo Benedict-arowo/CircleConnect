a49709176fead1088f362e1ed251c8c9d27fbe4e
Benedict <benedict.arowo@gmail.com>
2024-03-05 11:41:00 +0300

    Fix project reviews column name typo in Role table

--- server/prisma/migrations/20240305080626_project_reviews/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `canManageProjectRevies` on the `Role` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Role" DROP COLUMN "canManageProjectRevies",
-ADD COLUMN     "canManageProjectReviews" BOOLEAN NOT NULL DEFAULT false;
