a285eb36b9b516a3513d0d68a79f41084c517844
Benedict <benedict.arowo@gmail.com>
2024-03-06 23:10:17 +0300

    Add Vercel configuration to cors

--- server/index.ts
+++ server/index.ts
@@ -38,7 +38,7 @@ const makeApp = (
 	const io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap> =
 		new Server(server, {
 			cors: {
-				origin: "http://localhost:5173",
+				origin: "https://circle-connect-ruddy.vercel.app",
 				credentials: true,
 			},
 		});
@@ -66,7 +66,11 @@ const makeApp = (
 	app.use("", morgan("dev"));
 	app.use(
 		cors({
-			origin: ["http://localhost:5173", "http://127.0.0.1:5500"],
+			origin: [
+				"http://localhost:5173",
+				"http://127.0.0.1:5500",
+				"https://circle-connect-ruddy.vercel.app",
+			],
 			credentials: true,
 		})
 	);
