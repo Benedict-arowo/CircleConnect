f354abcc397eddf389ab9181b9572218059758f2
Benedict <benedict.arowo@gmail.com>
2024-03-08 05:45:56 +0300

    Removed material ui

--- client/src/pages/Test.tsx
+++ client/src/pages/Test.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../slices/userSlice";
 // import { increment, decrement } from "./Auth/userSlice";
